bb6e84effe1b7a9bf254ad56b384c895935f6035
AzmiHabib <azmihabib0323@gmail.com>
2022-04-08 01:59:20 +0600

    error massege shows on ui

--- src/App.js
+++ src/App.js
@@ -7,54 +7,63 @@ import { useState } from "react";
 
 const auth = getAuth(app);
 
-
 function App() {
-
-  const [email,setEmail] = useState('');
-  const [password,setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [validated, setValidated] = useState(false);
-  
+  const [error, setError] = useState("");
 
   const handleEmail = (event) => {
     setEmail(event.target.value);
   };
 
-
   const handlePassword = (event) => {
     setPassword(event.target.value);
   };
 
-
   const handleSubmit = (event) => {
-
+    event.preventDefault();
     const form = event.currentTarget;
     if (form.checkValidity() === false) {
-      event.preventDefault();
       event.stopPropagation();
+      return;
     }
-
+    if (!/(?=.*[A-Z])/.test(password)) {
+      setError('password should contain at least one uppercase character')
+      return;
+    }
     setValidated(true);
-    
+    setError('')
+
     createUserWithEmailAndPassword(auth, email, password)
-    .then(result=>{
-      const user = result.user;
-      console.log(user);
-    })
-    .catch(error=>{
-      console.error(error);
-    })
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
     event.preventDefault();
   };
-
 
   return (
     <div>
       <div className="registration w-50 mx-auto mt-2">
         <h2 className="text-primary">Please Register!!</h2>
-        <Form noValidate validated={validated} onSubmit={handleSubmit} className="">
+        <Form
+          noValidate
+          validated={validated}
+          onSubmit={handleSubmit}
+          className=""
+        >
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
-            <Form.Control onBlur={handleEmail} type="email" placeholder="Enter email" required/>
+            <Form.Control
+              onBlur={handleEmail}
+              type="email"
+              placeholder="Enter email"
+              required
+            />
             <Form.Control.Feedback type="invalid">
               Please provide a valid email.
             </Form.Control.Feedback>
@@ -65,14 +74,21 @@ function App() {
 
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control onBlur={handlePassword} type="password" placeholder="Password" required/>
+            <Form.Control
+              onBlur={handlePassword}
+              type="password"
+              placeholder="Password"
+              required
+            />
             <Form.Control.Feedback type="invalid">
               Please provide a valid password.
             </Form.Control.Feedback>
           </Form.Group>
+          <p className="text-danger">{error}</p>
           <Form.Group className="mb-3" controlId="formBasicCheckbox">
             <Form.Check type="checkbox" label="Check me out" />
           </Form.Group>
+          
           <Button variant="primary" type="submit">
             Submit
           </Button>
