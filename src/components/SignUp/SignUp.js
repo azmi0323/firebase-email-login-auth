3d4fa4d7102fbdf5f97646a7eebe7d1282ae7908
AzmiHabib <azmihabib0323@gmail.com>
2022-04-15 23:55:30 +0600

    style

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,11 +1,11 @@
-import React from 'react';
-
+import React from "react";
+import "./SignUp.css";
 const SignUp = () => {
-    return (
-        <div>
-            <h3>This is SignUp page</h3>
-        </div>
-    );
+  return (
+    <div>
+      <h3>This is SignUp page</h3>
+    </div>
+  );
 };
 
 export default SignUp;
